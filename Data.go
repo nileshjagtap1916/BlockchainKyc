1bf49cfb93646cd0c69ba47b81e6e78c9ba66981
nijagtap <nilesh.jagtap@capgemini.com>
2017-03-09 15:07:28 +0530

    test

--- Data.go
+++ Data.go
@@ -1,10 +1,10 @@
 package main
 
 type KycData struct {
-	UserId        string `json:"UserId"`
-	BankName      string `json:"BankName"`
-	UserName      string `json:"UserName"`
-	CreateDate    string `json:"CreateDate"`
-	ValidTillDate string `json:"ValidTillDate"`
-	KycDocument   string `json:"KycDocument"`
+	USER_NAME           string `json:"USER_NAME"`
+	USER_ID             string `json:"USER_ID"`
+	KYC_BANK_NAME       string `json:"KYC_BANK_NAME"`
+	KYC_CREATE_DATE     string `json:"KYC_CREATE_DATE"`
+	KYC_VALID_TILL_DATE string `json:"KYC_VALID_TILL_DATE"`
+	KYC_DOC_BLOB        string `json:"KYC_DOC_BLOB"`
 }
