1bcb688c2a98319f2c158ab2e5b95c50dddcc7f9
nijagtap <nilesh.jagtap@capgemini.com>
2017-03-09 15:07:37 +0530

    test

--- DataAccessLayer.go
+++ DataAccessLayer.go
@@ -12,13 +12,13 @@ func CreateDatabase(stub shim.ChaincodeStubInterface) error {
 	var err error
 
 	//Create table "tblKycDetails"
-	err = stub.CreateTable("KycDetails", []*shim.ColumnDefinition{
-		&shim.ColumnDefinition{Name: "UserId", Type: shim.ColumnDefinition_STRING, Key: true},
-		&shim.ColumnDefinition{Name: "BankName", Type: shim.ColumnDefinition_STRING, Key: true},
-		&shim.ColumnDefinition{Name: "UserName", Type: shim.ColumnDefinition_STRING, Key: false},
-		&shim.ColumnDefinition{Name: "CreateDate", Type: shim.ColumnDefinition_STRING, Key: false},
-		&shim.ColumnDefinition{Name: "ValidTillDate", Type: shim.ColumnDefinition_STRING, Key: false},
-		&shim.ColumnDefinition{Name: "KycDocument", Type: shim.ColumnDefinition_STRING, Key: false},
+	err = stub.CreateTable("tblKycDetails", []*shim.ColumnDefinition{
+		&shim.ColumnDefinition{Name: "USER_ID", Type: shim.ColumnDefinition_STRING, Key: true},
+		&shim.ColumnDefinition{Name: "KYC_BANK_NAME", Type: shim.ColumnDefinition_STRING, Key: true},
+		&shim.ColumnDefinition{Name: "USER_NAME", Type: shim.ColumnDefinition_STRING, Key: false},
+		&shim.ColumnDefinition{Name: "KYC_CREATE_DATE", Type: shim.ColumnDefinition_STRING, Key: false},
+		&shim.ColumnDefinition{Name: "KYC_VALID_TILL_DATE", Type: shim.ColumnDefinition_STRING, Key: false},
+		&shim.ColumnDefinition{Name: "KYC_DOC_BLOB", Type: shim.ColumnDefinition_STRING, Key: false},
 	})
 	if err != nil {
 		return errors.New("Failed creating KycDetails table.")
@@ -36,9 +36,12 @@ func CreateDatabase(stub shim.ChaincodeStubInterface) error {
 	return nil
 }
 
-func InsertKYCDetails(stub shim.ChaincodeStubInterface, args []string) (bool, error) {
+func InsertKYCDetails(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	var err error
-	var ok bool
+
+	if len(args) != 4 {
+		return nil, errors.New("Incorrect number of arguments. Need 4 arguments")
+	}
 
 	UserId := args[0]
 	BankName := args[1]
@@ -48,7 +51,7 @@ func InsertKYCDetails(stub shim.ChaincodeStubInterface, args []string) (bool, er
 	CreateDate := CurrentDate.Format("02-01-2006")
 	ValidTillDate := CurrentDate.AddDate(2, 0, 0).Format("02-01-2006")
 
-	ok, err = stub.InsertRow("KycDetails", shim.Row{
+	ok, err := stub.InsertRow("tblKycDetails", shim.Row{
 		Columns: []*shim.Column{
 			&shim.Column{Value: &shim.Column_String_{String_: UserId}},
 			&shim.Column{Value: &shim.Column_String_{String_: BankName}},
@@ -58,10 +61,14 @@ func InsertKYCDetails(stub shim.ChaincodeStubInterface, args []string) (bool, er
 			&shim.Column{Value: &shim.Column_String_{String_: KycDoc}},
 		},
 	})
-	return ok, err
+
+	if !ok && err == nil {
+		return nil, errors.New("Error in adding KYC record.")
+	}
+	return nil, nil
 }
 
-func InsertBankDetails(stub shim.ChaincodeStubInterface, BankName string, UserList []string) (bool, error) {
+/*func InsertBankDetails(stub shim.ChaincodeStubInterface, BankName string, UserList []string) ([]byte, error) {
 	var err error
 	var ok bool
 	JsonAsBytes, _ := json.Marshal(UserList)
@@ -71,34 +78,46 @@ func InsertBankDetails(stub shim.ChaincodeStubInterface, BankName string, UserLi
 			&shim.Column{Value: &shim.Column_Bytes{Bytes: JsonAsBytes}},
 		},
 	})
-	return ok, err
-}
+	if !ok && err == nil {
+		return nil, errors.New("Error in adding KYC record.")
+	}
+	return nil, nil
+}*/
 
-func GetKYCDetails(stub shim.ChaincodeStubInterface, UserId string, BankName string) (KycData, error) {
-	var KYCDetails KycData
+func GetKYCDetails(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var KycDataObj KycData
+	//var jsonAsBytes []byte
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting UserId and UserBank to query")
+	}
+
+	UserId := args[0]
+	UserBank := args[1]
+
 	var columns []shim.Column
 
 	col1 := shim.Column{Value: &shim.Column_String_{String_: UserId}}
-	col2 := shim.Column{Value: &shim.Column_String_{String_: BankName}}
+	col2 := shim.Column{Value: &shim.Column_String_{String_: UserBank}}
 	columns = append(columns, col1)
 	columns = append(columns, col2)
 
-	row, err := stub.GetRow("KYCDetails", columns)
+	row, err := stub.GetRow("tblKycDetails", columns)
 	if err != nil {
-		return KYCDetails, errors.New("Failed to query table ContractDetails")
+		return nil, errors.New("Failed to query")
 	}
 
-	KYCDetails.UserId = row.Columns[0].GetString_()
-	KYCDetails.BankName = row.Columns[1].GetString_()
-	KYCDetails.UserName = row.Columns[2].GetString_()
-	KYCDetails.CreateDate = row.Columns[3].GetString_()
-	KYCDetails.ValidTillDate = row.Columns[4].GetString_()
-	KYCDetails.KycDocument = row.Columns[5].GetString_()
+	KycDataObj.USER_ID = row.Columns[0].GetString_()
+	KycDataObj.KYC_BANK_NAME = row.Columns[1].GetString_()
+	KycDataObj.USER_NAME = row.Columns[2].GetString_()
+	KycDataObj.KYC_CREATE_DATE = row.Columns[3].GetString_()
+	KycDataObj.KYC_VALID_TILL_DATE = row.Columns[4].GetString_()
+	KycDataObj.KYC_DOC_BLOB = row.Columns[5].GetString_()
 
-	return KYCDetails, nil
+	jsonAsBytes, _ := json.Marshal(KycDataObj)
+	return jsonAsBytes, nil
 }
 
-func GetUserList(stub shim.ChaincodeStubInterface, BankName string) ([]string, error) {
+/*func GetUserList(stub shim.ChaincodeStubInterface, BankName string) ([]string, error) {
 	var UserList []string
 	var columns []shim.Column
 
@@ -114,9 +133,9 @@ func GetUserList(stub shim.ChaincodeStubInterface, BankName string) ([]string, e
 	json.Unmarshal(UsersAsBytes, &UserList)
 
 	return UserList, nil
-}
+}*/
 
-func UpdateBankDetails(stub shim.ChaincodeStubInterface, BankName string, Userlist []string) (bool, error) {
+/*func UpdateBankDetails(stub shim.ChaincodeStubInterface, BankName string, Userlist []string) (bool, error) {
 
 	JsonAsBytes, _ := json.Marshal(Userlist)
 
@@ -131,4 +150,4 @@ func UpdateBankDetails(stub shim.ChaincodeStubInterface, BankName string, Userli
 		return false, errors.New("Error in updating Bank record.")
 	}
 	return true, nil
-}
+}*/
