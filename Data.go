d944a0a52e0ecdcf3d07ab8283f796094faa14ea
nijagtap <nilesh.jagtap@capgemini.com>
2017-04-25 12:13:38 +0530

    extra 4 fields per bank

--- Data.go
+++ Data.go
@@ -1,16 +1,16 @@
 package main
 
 type KycData struct {
-	USER_NAME           string `json:"USER_NAME"`
-	USER_ID             string `json:"USER_ID"`
-	KYC_BANK_NAME       string `json:"KYC_BANK_NAME"`
-	KYC_CREATE_DATE     string `json:"KYC_CREATE_DATE"`
-	KYC_VALID_TILL_DATE string `json:"KYC_VALID_TILL_DATE"`
-	KYC_DOC_BLOB        string `json:"KYC_DOC_BLOB"`
-	KYC_INFO_1          string `json:"KYC_INFO_1"`
-	KYC_INFO_2          string `json:"KYC_INFO_2"`
-	KYC_INFO_3          string `json:"KYC_INFO_3"`
-	KYC_INFO_4          string `json:"KYC_INFO_4"`
+	USER_NAME           string `json:"userName"`
+	USER_ID             string `json:"userId"`
+	KYC_BANK_NAME       string `json:"kycBankName"`
+	KYC_CREATE_DATE     string `json:"kycCreateDate"`
+	KYC_VALID_TILL_DATE string `json:"kycValidTillDate"`
+	KYC_DOC_BLOB        string `json:"kycDocBlob"`
+	KYC_INFO_1          string `json:"kycInfo1"`
+	KYC_INFO_2          string `json:"kycInfo2"`
+	KYC_INFO_3          string `json:"kycInfo3"`
+	KYC_INFO_4          string `json:"kycInfo4"`
 }
 
 type KycCount struct {
